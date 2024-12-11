ed00a0052ee7bc884b7858856d96f1aca0af19aa
mmatejczuk <01171250@pw.edu.pl>
2024-12-11 01:03:53 +0100

    configure AIPlayer to use weights from hugging face

--- PackitAIPlayer.py
+++ PackitAIPlayer.py
@@ -1,35 +1,97 @@
 import numpy as np
 from HexGame.HexGame import *
 from HexGame.keras.NNet import NNetWrapper as HexNet
+from HexGame.keras.NNetSmall import NNetWrapper as HexNetSmall
 from TriangleGame.TriangleGame import *
 from TriangleGame.keras.NNet import NNetWrapper as TriNet
+from TriangleGame.keras.NNetSmall import NNetWrapper as TriNetSmall
+import tensorflow as tf
 from PackitMCTS import MCTS
 from utils import *
 import logging
+import os
 
 log = logging.getLogger(__name__)
  
+
+
 class AIPlayer():
 
     def __init__(self, size, mode = 'triangular'):
 
-        args = dotdict({'numMCTSSims': 10, 'cpuct': 1.0})
+        mcts_args = dotdict({'numMCTSSims': 10,
+                        'cpuct': 1.0})
 
-        #this if clause can be better
+        args = dotdict({'tri_weights_folder': './packit-polygons-models/triangle_models/',
+                        'hex_weights_folder': './packit-polygons-models/hex_models/',
+                        'weights_filename': 'best.weights.h5',
+                        'tri_hf_weights_path': 'https://huggingface.co/lgfn/packit-polygons-models/resolve/main/triangle_models/',
+                        'hex_hf_weights_path': 'https://huggingface.co/lgfn/packit-polygons-models/resolve/main/hex_models/'})
+
+
         if mode == 'triangular':
             self.game = TriangleGame(size)
+            args.tri_weights_folder += 'size_'+str(size)+'/'
+            
+            local_weights_path = args.tri_weights_folder + args.weights_filename
 
-            #random model for now
-            self.nnet = TriNet(self.game)
+            if size < 5:
+                self.nnet = TriNetSmall(self.game)
+            else:
+                self.nnet = TriNet(self.game)
+
+            if not os.path.isfile(local_weights_path):
+                
+                print('Local weights file not found')
+
+                weights_path = args.tri_hf_weights_path + 'size_'+str(size)+'/' + args.weights_filename
+                try:
+                    print('Downloading weights...')
+                    print('Accessing ', weights_path)
+                    self.nnet.nnet.model.load_weights(tf.keras.utils.get_file(args.weights_filename, weights_path))
+                    self.nnet.save_checkpoint(folder=args.tri_weights_folder, filename=args.weights_filename)
+                except Exception as e:
+                    print(e)
+                    print('Weights not found, using untrained model')
+                
+
+            else:
+                self.nnet.load_checkpoint(folder=args.tri_weights_folder, filename=args.weights_filename)
+            
+
         elif mode == 'hexagonal':
             self.game = HexGame(size)
+            args.hex_weights_folder += 'size_'+str(size)+'/'
 
-            #random model for now
-            self.nnet = HexNet(self.game)
+            local_weights_path = args.hex_weights_folder + args.weights_filename
+
+            if size < 3:
+                self.nnet = HexNetSmall(self.game)
+            else:
+                self.nnet = HexNet(self.game)
+
+            if not os.path.isfile(local_weights_path):
+
+                print('Local weights file not found')
+
+                weights_path = args.hex_hf_weights_path + 'size_'+str(size)+'/' + args.weights_filename
+
+                try:
+                    print('Downloading weights...')
+                    print('Accessing ', weights_path)
+
+                    self.nnet.nnet.model.load_weights(tf.keras.utils.get_file(args.weights_filename, weights_path))
+                    self.nnet.save_checkpoint(folder=args.hex_weights_folder, filename=args.weights_filename)
+                except Exception as e :
+                    print(e)
+                    print("Weights not found, using untrained model")
+
+            else:
+                self.nnet.load_checkpoint(folder=args.hex_weights_folder, filename=args.weights_filename)
         else:
             raise Exception('Invalid mode')
 
-        self.mcts = MCTS(self.game,self.nnet, args)
+        self.mcts = MCTS(self.game,self.nnet, mcts_args)
 
     def mcts_get_action(self, board, turn):
         '''
